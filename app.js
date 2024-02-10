d93eff6879cc2c0dd11a5aece1cd29afeb2eed13
Matt Daniel Brown <mattbrown@onmail.com>
2024-02-09 19:27:48 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,4 @@
+'use strict';
+// console.log('hello');
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
---
+++ src/javascripts/carrier-lookup.js
@@ -0,0 +1,81 @@
+'use strict';
+
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+// Global variable to store the phone number carrier
+const accessKey = apiKey;
+let phoneNumber;
+let phoneNumberCarrier;
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+	// DOM Setup
+	const carrierLookupForm = document.getElementById('carrier-lookup-form');
+	const carrierLookupFormSubmit = document.getElementById('carrier-lookup-form-submit');
+	const carrierLookupResults = document.getElementById('carrier-lookup-result');
+	const phoneNumberInput = document.getElementById('phone-input');
+
+	function handleResult(result) {
+		console.log(result);
+		console.log("\n");
+		console.log(result.carrier);
+		carrierLookupResults.innerHTML = result.carrier;
+	}
+
+	function performApiFetch(url, resultsElement) {
+		// use the fetch method for the AJAX call
+
+		// 		fetch(url)
+		// 			.then(response => {
+		// 				if (!response.ok) {
+		// 					throw new Error('Network response was not ok');
+		// 				}
+		// 				return response.json();
+		// 			})
+		// 			.then(json => {
+		// 				// Store the phone number carrier in the global variable
+		// 				// phoneNumberCarrier = json.carrier;
+		// 
+		// 				// Access and use your preferred validation result objects
+		// 				console.log(`IS JSON VALID: ${json.valid}`);
+		// 				console.log(`COUNTRY CODE: ${json.country_code}`);
+		// 				console.log(`CARRIER: ${phoneNumberCarrier}`);
+		// 				// resultsElement.innerHTML = phoneNumberCarrier;
+		// 			})
+		// 			.catch(error => {
+		// 				console.error('There was a problem with the fetch operation:', error);
+		// 			});
+
+		const myHeaders = new Headers();
+		myHeaders.append("apikey", accessKey);
+
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow',
+			headers: myHeaders
+		};
+
+		fetch(url, requestOptions)
+			.then(response => response.text())
+			.then(result => handleResult(result))
+			.catch(error => console.log('error', error));
+	}
+
+
+	carrierLookupForm.addEventListener('submit', function (event) {
+		event.preventDefault();
+
+		phoneNumber = phoneNumberInput.value;
+		// const url = `http://apilayer.net/api/validate?access_key=${accessKey}&number=${phoneNumber}`;
+		const url = `http://api.apilayer.com/number_verification/validate?number=${phoneNumber}`;
+
+		performApiFetch(url, carrierLookupResults);
+	});
+
+
+
+
+
+
+
+});
